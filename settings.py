fd7f143c2ce0d7d1d75e2a85d5ad02aca038dcc4
Alexis Prunier <alexis@ubuntu.(none)>
2013-04-13 04:32:14 -0700

    Add fucking design

--- settings.py
+++ settings.py
@@ -64,10 +64,11 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/home/alexis/Desktop/workspace/Geekhub/templates/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    '/home/alexis/Desktop/workspace/Geekhub/templates/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
