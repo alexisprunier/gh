80d06f09c335e7777531d61dfad63ba76bc4d9bd
alexisprunier <prunier.al@gmail.com>
2013-05-14 07:45:25 -0700

    add news !

--- GeekHub/views.py
+++ GeekHub/views.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render
-from GeekHub.models import Article
+from GeekHub.models import Article, Facebook
 
 def accueil(request):
     
@@ -29,7 +29,25 @@ def news(request, page_number):
         
     return render(request, 'news.html', locals())
 
-def facebook(request):
+def facebook(request, page_number):
+    
+    # recuperation des articles
+    all_articles = Facebook.objects.all().order_by("id")
+    size = Facebook.objects.all().count()
+    
+    # recuperation des pages
+    page_list = []
+    i = 1
+    while i <= (size-1)/10+1 :
+        page_list.append(i)
+        i += 1
+    
+    # selection des article de la page
+    selected_article = []
+    page_number = int(page_number.encode('ascii'))
+    for i in [1,2,3,4,5,6,7,8,9,10]:
+        if ( 0 <= size-(page_number*10-10+i) < size):
+            selected_article.append(all_articles[size-(page_number*10-10+i)])
     
     return render(request, 'facebook.html', locals())
 
--- urls.py
+++ urls.py
@@ -6,10 +6,9 @@ from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('GeekHub.views',
-    url(r'^', 'accueil'),
-    #url(r'^news/(\d{4})/?$', 'news'),
-    url(r'^news/', 'news'),
-    url(r'^facebook/', 'facebook'),
-    url(r'^twitter/', 'twitter'),
+    url(r'^accueil/', 'accueil'),
+    url(r'^news/(\d{4})/?$', 'news'),
+    url(r'^facebook/(\d{4})/?$', 'facebook'),
+    url(r'^twitter/(\d{4})/?$', 'twitter'),
     url(r'^admin/', include(admin.site.urls)),
 )
