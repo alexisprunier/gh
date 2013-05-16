e225d625a2b2fd073a5ae6dfbc0c58f779202772
alexisprunier <prunier.al@gmail.com>
2013-05-16 08:01:50 -0700

    add twitter and fb on home

--- GeekHub/models.py
+++ GeekHub/models.py
@@ -7,6 +7,8 @@ class Article(models.Model):
     lien = models.TextField(null=True)
     date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
     image = models.CharField(max_length=200)
+    vote_pos = models.IntegerField(null=True)
+    vote_neg = models.IntegerField(null=True)
     
     def __unicode__(self):
         return self.titre
@@ -21,14 +23,28 @@ class Facebook(models.Model):
     contenu = models.TextField(null=True)
     lien = models.TextField(null=True)
     date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
-    image = models.CharField(max_length=200)
     
     def __unicode__(self):
         return self.titre
     
 class FacebookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'titre', 'origine', 'contenu', 'lien', 'date', 'image')
-    search_fields = ('id', 'titre', 'origine', 'contenu', 'lien', 'date', 'image')
+    list_display = ('id', 'titre', 'origine', 'contenu', 'lien', 'date')
+    search_fields = ('id', 'titre', 'origine', 'contenu', 'lien', 'date')
+    
+class Twitter(models.Model):
+    titre = models.CharField(max_length=150, unique=True)
+    origine = models.CharField(max_length=42)
+    contenu = models.TextField(null=True)
+    lien = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+    
+    def __unicode__(self):
+        return self.titre
+    
+class TwitterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titre', 'origine', 'contenu', 'lien', 'date')
+    search_fields = ('id', 'titre', 'origine', 'contenu', 'lien', 'date')
 
 admin.site.register(Facebook, FacebookAdmin)
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Twitter, TwitterAdmin)
--- GeekHub/views.py
+++ GeekHub/views.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render
-from GeekHub.models import Article, Facebook
+from GeekHub.models import Article, Facebook, Twitter
 
 def accueil(request):
     
@@ -51,6 +51,24 @@ def facebook(request, page_number):
     
     return render(request, 'facebook.html', locals())
 
-def twitter(request):
+def twitter(request, page_number):
+    
+    # recuperation des articles
+    all_articles = Twitter.objects.all().order_by("id")
+    size = Twitter.objects.all().count()
+    
+    # recuperation des pages
+    page_list = []
+    i = 1
+    while i <= (size-1)/10+1 :
+        page_list.append(i)
+        i += 1
+    
+    # selection des article de la page
+    selected_article = []
+    page_number = int(page_number.encode('ascii'))
+    for i in [1,2,3,4,5,6,7,8,9,10]:
+        if ( 0 <= size-(page_number*10-10+i) < size):
+            selected_article.append(all_articles[size-(page_number*10-10+i)])
     
     return render(request, 'twitter.html', locals())
