cbd87271a15db4ec57d0a1266d026607eb13d751
hautbois_sas <hautbois_sas@bitbucket.org>
2013-09-19 16:37:47 +0200

    Ajout des script(python) au projet les numerique, erreur d'import

---
+++ GeekHubScripts/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import LesNumeriques
+
+
+GLOBAL_nom_bdd = "GeekHubDatabase"
+GLOBAL_nb_infos = 2
+GLOBAL_user_bdd = "user"
+GLOBAL_pass_bdd = "pass"
+
+o_lesnums = LesNumeriques(nb_infos=GLOBAL_nb_infos)
+o_tomsguide = LesNumeriques(nb_infos=GLOBAL_nb_infos)
+
+
+
+
+       
+
+
+
