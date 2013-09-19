ee851638ba68b85d0de6bf9462b673c4850813c0
hautbois_sas <hautbois_sas@bitbucket.org>
2013-09-19 16:37:47 +0200

    Ajout des script(python) au projet les numerique, erreur d'import

---
+++ GeekHubScripts/LesNumeriques.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import urllib
+from GeekHub.models import Article
+
+class LesNumeriques:
+    def __init__(self,nb_infos):
+        self.url = 'http://feeds.feedburner.com/lesnumeriques/news'
+        self.source = "Les Numériques"
+        self.get_infos(nb_infos)
+    
+    def get_infos(self, nb_infos):
+        
+        html = urllib.urlopen(self.url).read()
+        page_web = BeautifulSoup(html)
+        
+        #print page_web.prettify() #Affichage source complete
+        
+        list_art = page_web.find_all("entry", limit=nb_infos)
+        for article in list_art :    
+            if  (article.find("content")) != None : #if not breves
+                #link
+                link = (article.find("link")).get("href")
+                #title
+                title = (article.find("title")).text        
+                #photo
+                content = BeautifulSoup((article.find("content")).text)
+                photo = content.find("img").get("src")
+                #BDD
+                bdd_article = Article(titre = title, lien = link, origine = self.source, image = photo )
+                bdd_article.save()
--- settings.py
+++ settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'GeekHubDatabase',                      # Or path to database file if using sqlite3.
-        'USER': 'alexis',                      # Not used with sqlite3.
-        'PASSWORD': 'ghct2lb',                  # Not used with sqlite3.
+        'NAME': 'GH',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'hautbois_2013',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
