fa6059c6056c556dbed226c6cdec7f953bab0f7d
hautbois_sas <hautbois_sas@bitbucket.org>
2013-09-24 13:44:43 +0200

    clean presse citron

--- GeekHubScripts/PresseCitron.py
+++ GeekHubScripts/PresseCitron.py
@@ -3,31 +3,30 @@ from bs4 import BeautifulSoup
 #from GeekHub.models import Article
 import urllib
 
-url = 'http://www.presse-citron.net/'
-source = "Presse Citron"
+class PresseCitron :
+    def __init__(self,nb_infos):
+        self.url = 'http://www.presse-citron.net/'
+        self.source = "Presse Citron"
+        self.get_infos(nb_infos)
 
-
-
-html = urllib.urlopen(url).read()
-page_web = BeautifulSoup(html)
-
-
-page_web = page_web.find("div", {"class":"posts posts-index"}) #Nettoyage, selection de la div des posts
-
-#print page_web.prettify() #Affichage source complete
-list_art = page_web.find_all("div",{"class":"hentry"}, limit=10)
-print list_art
-for article in list_art :    
-        #link
-        link = (article.find("h2", {"class":"post-title"})).find("a").get("href")  
-        #title
-        title =  (article.find("h2", {"class":"post-title"})).find("a").text  
-        print title   
-        #photo
-        photo = (article.find("p",{"class":"post-thumbnail"})).find("img").get("src")
-        #BDD
-        #bdd_article = Article(titre = title, lien = link, origine = self.source, image = photo )
-        #bdd_article.save()
-        print link
-       
-        print photo
+        
+        def get_infos(self, nb_infos):
+        
+            html = urllib.urlopen(self.url).read()
+            page_web = BeautifulSoup(html)
+            
+            page_web = page_web.find("div", {"class":"posts posts-index"}) #Nettoyage, selection de la div des posts
+            
+            list_art = page_web.find_all("div",{"class":"hentry"}, limit=10)
+            for article in list_art :    
+                if  (article.find("content")) != None : #if not breves
+                    #link
+                    link = (article.find("h2", {"class":"post-title"})).find("a").get("href")  
+                    #title
+                    title =  (article.find("h2", {"class":"post-title"})).find("a").text  
+                    #photo
+                    photo = (article.find("p",{"class":"post-thumbnail"})).find("img").get("src")
+    
+                    #BDD
+                    #bdd_article = Article(titre = title, lien = link, origine = self.source, image = photo )
+                    #bdd_article.save()
