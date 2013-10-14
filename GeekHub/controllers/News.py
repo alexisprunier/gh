bc131ddb7e2e2c8d1c4e620aaf81f9c327135431
alexisprunier <prunier.al@gmail.com>
2013-10-14 23:39:51 +0200

    avancement droite content et rafraichissement news

--- GeekHub/controllers/News.py
+++ GeekHub/controllers/News.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render
 from GeekHub.models import Article
+from GeekHub.controllers.Proxy import Proxy
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -13,6 +14,10 @@ def news(request, page_number):
     #page_prec = page_number-1 if page_number > 1 else page_number
     #page_suiv = page_number+1 if page_number < 9 else page_number
     
+    proxy = Proxy()
+    best_article = proxy.get_most_visited_article()
+    best_site = proxy.get_most_visited_site()
+    
     return render(request, 'news.html', locals())
 
 @csrf_exempt
@@ -23,17 +28,17 @@ def refresh(request):
     
     for article in selected_articles:
         content += "\
-                <a href='" + str(article.lien) + "'>\
+                <a href='" + unicode(article.lien) + "'>\
                 <div id='article'>\
-                    <object id='img_art' type='image/jpeg' data='" + str(article.image) + "'>\
-                        <object id='img_art2' type='image/jpeg' data='" + str(request.POST.get('static_url', False)) + "image/empty_image.png'></object>\
+                    <object id='img_art' type='image/jpeg' data='" + unicode(article.image) + "'>\
+                        <object id='img_art2' type='image/jpeg' data='" + unicode(request.POST.get('static_url', False)) + "image/empty_image.png'></object>\
                     </object>\
                     <div id='info_art'>\
                         <div id='titre_date'>\
-                            <div id='titre_art'>" + str(article.titre) + "</div>\
-                            <div id='date_art'>" + str(article.date) + "</div>\
+                            <div id='titre_art'>" + unicode(article.titre) + "</div>\
+                            <div id='date_art'>" + unicode(article.date) + "</div>\
                         </div>\
-                        <div id='origine_art'><img id='favicon' src='" + str(request.POST.get('static_url', False)) + "favicon/" + str(article.origine) + ".png' align='bottom' alt='" + str(request.POST.get('static_url', False)) + "image/empty_image.png'></img><div id='text_article'>" + str(article.origine) + "</div></div>\
+                        <div id='origine_art'><img id='favicon' src='" + str(request.POST.get('static_url', False)) + unicode(article.origine) + ".png' align='bottom' alt='" + unicode(request.POST.get('static_url', False)) + "image/empty_image.png'></img><div id='text_article'>" + unicode(article.origine) + "</div></div>\
                     </div>\
                 </div>\
                 </a>"
